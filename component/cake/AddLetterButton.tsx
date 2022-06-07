d1933dbc6e5d56fe26b69672f63ba8f34515c633
Nanjangpan <b970311@yonsei.ac.kr>
2022-06-07 22:51:17 +0900

    feat(cake): add cake page prototype

---
+++ component/cake/AddLetterButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type IAddLetterButtonProps = {
+	isArticle: boolean;
+};
+
+export const AddLetterButton: React.FC<IAddLetterButtonProps> = ({
+	isArticle,
+}) => {
+	return <button disabled={isArticle}>버튼1</button>;
+};
---
+++ pages/cake/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next';
+import Image from 'next/image';
+
+// TODO: 절대경로로 바꾸기
+import { AddLetterButton } from '../../component/cake/AddLetterButton';
+import styles from '../../styles/Cake.module.css';
+
+const Cake: NextPage = () => {
+	const isArticle = true;
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+				<div className={styles.cake}>
+					<Image src="/images/cake.png" alt="cake" height={500} width={500} />
+				</div>
+				<div className={styles.button}>
+					<AddLetterButton isArticle={isArticle} />
+				</div>
+			</main>
+		</div>
+	);
+};
+
+export default Cake;
