124f122ccf05a16f105c48e28df026e2c63ec0e1
SourLemon <joice9559@gmail.com>
2022-06-22 23:05:51 +0900

    feat(cake): ddaycount

--- component/cake/DdayCount.tsx
+++ component/cake/DdayCount.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-type Birthday = {
+type dDay = {
 	Birthday: string;
+	Today: string;
 };
 
-export const DdayCount: React.FC<Birthday> = ({ Birthday }) => {
+export const DdayCount: React.FC<dDay> = ({ Birthday, Today }) => {
 	const BirthdayDate = new Date(Birthday);
-	const TodayDate = new Date();
-	const DateDiff = Math.abs(BirthdayDate.getTime() - TodayDate.getTime());
+	const TodayDate = new Date(Today);
+	const DateDiff = Math.abs((BirthdayDate.getTime() - TodayDate.getTime()) / (1000 * 60 * 60 * 24));
 	return (
 		<div>
-			D{BirthdayDate < TodayDate ? '-' : '+'}
-			{DateDiff}
+			D{BirthdayDate > TodayDate ? '-' : '+'}
+			{Math.floor(DateDiff)}
 		</div>
 	);
 	//return <button disabled={isArticle}>버튼1</button>;
