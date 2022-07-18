479658c40eab2bb7c169b18723d75fa2966abeca
Hakjun Lee <hakjun_lee@tmax.co.kr>
2022-07-18 14:40:12 +0900

    feat: 🎸 eslint & prettier & pre-commit hook
    ✅ Closes: 3
    
    style(pages): lint & prettier to existing files

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	printWidth: 100,
+	semi: true,
+	trailingComma: 'all',
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+	tabWidth: 2,
+	singleQuote: true,
+};
--- pages/cake/index.tsx
+++ pages/cake/index.tsx
@@ -9,27 +9,27 @@ const now = new Date();
 const bday = new Date('2022-12-25');
 
 const Cake: NextPage = () => {
-	const isArticle = true;
-	return (
-		<div className={styles.container}>
-			<main className={styles.main}>
-				<div className={styles.dday}>
-					<div>Today: {now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()}</div>
-					<div>Bday: {bday.getFullYear() + '-' + (bday.getMonth() + 1) + '-' + bday.getDate()}</div>
-					<DdayCount
-						Birthday={bday.getFullYear() + '-' + (bday.getMonth() + 1) + '-' + bday.getDate()}
-						Today={now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()}
-					></DdayCount>
-				</div>
-				<div className={styles.cake}>
-					<Image src="/images/cake.png" alt="cake" height={500} width={500} />
-				</div>
-				<div className={styles.button}>
-					<AddLetterButton isArticle={isArticle} />
-				</div>
-			</main>
-		</div>
-	);
+  const isArticle = true;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.dday}>
+          <div>Today: {now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()}</div>
+          <div>Bday: {bday.getFullYear() + '-' + (bday.getMonth() + 1) + '-' + bday.getDate()}</div>
+          <DdayCount
+            Birthday={bday.getFullYear() + '-' + (bday.getMonth() + 1) + '-' + bday.getDate()}
+            Today={now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()}
+          ></DdayCount>
+        </div>
+        <div className={styles.cake}>
+          <Image src="/images/cake.png" alt="cake" height={500} width={500} />
+        </div>
+        <div className={styles.button}>
+          <AddLetterButton isArticle={isArticle} />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Cake;
