ce57097514797d36dba6d152797e168569905245
gyurin.hwang <gyurin.hwang@navercorp.com>
2022-07-24 21:43:28 +0900

    #2 WIP [Sidebar] 기본 틀 생성 - apply styled components

--- component/common/Sidebar.tsx
+++ component/common/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { useReducer } from 'react';
-import styles from '../../styles/sidebar.module.css';
+import styled from 'styled-components';
 
 function toggleMenu(isMenuOpen: boolean) {
   return !isMenuOpen;
@@ -8,23 +8,51 @@ function toggleMenu(isMenuOpen: boolean) {
 export const Sidebar = () => {
   const [isMenuOpen, dispatch] = useReducer(toggleMenu, false);
 
+  const imgPath = './images/button/sidebar-button.png';
+
   return (
     <div>
-      <button type="button" className={styles.openButton} onClick={() => dispatch()}>
-        <span>OOO</span>
-      </button>
-      <div className={isMenuOpen ? styles.open : styles.close}>
-        <button type="button" className={styles.closeButton} onClick={() => dispatch()}>
-          <span>XXX</span>
-        </button>
-        <div className={styles.content}>
+      <Button onClick={() => dispatch()}>
+        <img src={imgPath} width="40px" />
+      </Button>
+      <Container isMenuOpen={isMenuOpen}>
+        <Button onClick={() => dispatch()}>
+          <img src={imgPath} width="40px" />
+        </Button>
+        <Content>
           <ul>
             <li>New Item</li>
             <li>New Item</li>
             <li>New Item</li>
           </ul>
-        </div>
-      </div>
+        </Content>
+      </Container>
     </div>
   );
 };
+
+const Container = styled.div<{ isMenuOpen: boolean }>`
+  width: 200px;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: -200px;
+  -webkit-transform: ${props => (props.isMenuOpen ? 'translateX(0)' : 'translateX(-200px)')};
+  transform: ${props => (props.isMenuOpen ? 'translateX(0)' : 'translateX(-200px)')};
+  -webkit-transition: 0.3s ease all;
+  transition: 0.3s ease all;
+  background: ${props => (props.isMenuOpen ? 'red' : 'yellowgreen')};
+  z-index: 100;
+`;
+
+const Button = styled.span`
+  top: 10px;
+  right: 10px;
+  position: absolute;
+`;
+
+const Content = styled.div`
+  background: blueviolet;
+  position: relative;
+  top: 60px;
+`;
