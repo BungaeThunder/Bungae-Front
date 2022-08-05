54ed33ea6486cb1d80b5de7064893791b28ad070
SourLemon <joice9559@gmail.com>
2022-08-05 22:32:19 +0900

    feat(colorpalette): 기획에쓰인 색상 팔레트

---
+++ public/styles/colorPalette.tsx
@@ -0,0 +1,12 @@
+// https://juzero-space.tistory.com/266?category=992170
+const pallette = {
+  black: '#292929',
+  pink: '#FC4062',
+  skyblue: '#6AC4D9',
+  lime: '#CDE185',
+  yellow: '#F2E56E',
+  purple: '#C5A2FE',
+  gray: '#C1C1C1',
+};
+
+export default pallette;
