a9106c417c1cb60eb4735ac91f98866ae2fbd974
Nanjangpan <b970311@yonsei.ac.kr>
2022-08-25 09:42:25 +0900

    feat(MyPageModal): fix css

--- component/cake/MyPageModal.tsx
+++ component/cake/MyPageModal.tsx
@@ -2,37 +2,74 @@ import React from 'react';
 import Modal from 'react-modal';
 import styled from 'styled-components';
 
-import Line from "public/images/line_1.svg";
+import Line from 'public/images/line_1.svg';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 type IMyPageModalProps = {
-    isModalOpen: boolean,
-    closeModal: () => void
+  isModalOpen: boolean;
+  closeModal: () => void;
 };
 
-export const MyPageModal: React.FC<IMyPageModalProps> = ({isModalOpen}) => {
-  const imgPath = "/images/profile_img.png"
+type IMyPageInputs = {
+  name: string;
+  date: Date;
+};
 
-	return (
+export const MyPageModal: React.FC<IMyPageModalProps> = ({ isModalOpen }) => {
+  const imgPath = '/images/profile_img.png';
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IMyPageInputs>();
+  const onSubmit: SubmitHandler<IMyPageInputs> = data => console.log(data);
+
+  return (
     <Modal
       isOpen={isModalOpen}
       // style={customStyles}
       className="_"
       overlayClassName="_"
-      contentElement={(props, children) => (
-        <ModalStyle {...props}>{children}</ModalStyle>
-      )}
+      contentElement={(props, children) => <ModalStyle {...props}>{children}</ModalStyle>}
       overlayElement={(props, contentElement) => (
         <OverlayStyle {...props}>{contentElement}</OverlayStyle>
       )}
     >
       <Title>My Page</Title>
       <TitleLine />
-      {/* TODO: make component */}
       <ProfileCircle>
         <ProfileImage src={imgPath} />
       </ProfileCircle>
+      <FormDiv>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormInput {...register('name', { required: true })} placeholder=" Name" />
+          <div>
+            <Controller
+              control={control}
+              name="date"
+              render={({ field }) => (
+                <DateForm
+                  placeholderText=" The Date"
+                  onChange={date => field.onChange(date)}
+                  selected={field.value}
+                />
+              )}
+            />
+          </div>
+          {/* errors ㄴill return when field validation fails  */}
+          {errors.name && <span>This field is required</span>}
+          <DivDdayText>
+            <DdayText>* d-day ??? *</DdayText>
+          </DivDdayText>
+          <button type="submit">Apply</button>
+        </form>
+      </FormDiv>
     </Modal>
-	);
+  );
 };
 
 // h2's font size is 30 px
@@ -40,37 +77,70 @@ export const MyPageModal: React.FC<IMyPageModalProps> = ({isModalOpen}) => {
 const Title = styled.h2`
   color: #000000;
   text-align: center;
-  font-family: "Yanone Kaffeesatz";
+  font-family: 'Yanone Kaffeesatz';
   font-style: normal;
   font-weight: 400;
   line-height: 30px;
   margin-top: 72px;
-`
+`;
 
 const TitleLine = styled(Line)`
   display: flex;
   justify-content: center;
   align-items: center;
   margint-top: 10px;
-`
+`;
 
 const ProfileCircle = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: #C5A2FE;
+  background: #c5a2fe;
   width: 78px;
-  height: 78px; 
+  height: 78px;
   border-radius: 70%;
   overflow: hidden;
   margin-top: 34px;
-`
+`;
 
 const ProfileImage = styled.img`
   width: 45px;
   height: 45px;
   object-fit: cover;
-`
+`;
+
+const FormDiv = styled.form`
+  margin-top: 34px;
+`;
+
+const FormInput = styled.input`
+  width: 242px;
+  height: 25px;
+  margin-bottom: 13px;
+  border: none;
+  border-radius: 8px;
+`;
+
+const DateForm = styled(DatePicker)`
+  width: 242px;
+  height: 25px;
+  margin-bottom: 10px;
+  border: none;
+  border-radius: 8px;
+`;
+
+const DivDdayText = styled.div`
+  margin-bottom: 101px;
+`;
+
+const DdayText = styled.text`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 18px;
+  color: #292929;
+`;
 
 const ModalStyle = styled.div`
   width: 326px;
@@ -79,9 +149,9 @@ const ModalStyle = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #E7E7E7;
+  background-color: #e7e7e7;
   border-radius: 0.25rem;
-  transition : all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
 `;
 
