1d1182a0d0d067d46d7441111231392d8e1044fe
gyurinida <gyurinida@naver.com>
2023-04-30 17:42:25 +0900

    #23 open menu with new menu button

--- component/common/Menu.tsx
+++ component/common/Menu.tsx
@@ -1,176 +1,28 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import MenuIcon from 'public/images/button/menu_button.svg';
-import ProfileIcon from 'public/images/button/menu_button.svg';
-import LetterIcon from 'public/images/button/letter.svg';
-import LetterDoneIcon from 'public/images/button/letter_done.svg';
-import SupportIcon from 'public/images/button/support_us_button.svg';
 
-export const Menu = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-
-  const side = useRef<HTMLInputElement>(null);
-  const sideBtn = useRef<HTMLInputElement>(null);
-
-  const handleClose = (event: MouseEvent) => {
-    const target = event.target as HTMLButtonElement;
-    const sideChildren = side.current?.contains(target);
-    const sideBtnChildren = sideBtn.current?.firstChild?.contains(target);
-
-    // 메뉴 열린 상태에서 메뉴 버튼 외부영역을 누르면, 메뉴를 닫는다.
-    isMenuOpen && !sideChildren && setIsMenuOpen(false);
-
-    // 메뉴 버튼을 누르면 메뉴를 오픈한다.
-    !!sideBtnChildren && setIsMenuOpen(true);
-  };
-
-  useEffect(() => {
-    isMenuOpen && window.addEventListener('click', handleClose);
-    return () => {
-      window.removeEventListener('click', handleClose);
-    };
-  });
-
-  return (
-    <div>
-      <MenuButtonSpan ref={sideBtn} onClick={() => setIsMenuOpen(true)}>
-        <MenuIcon />
-      </MenuButtonSpan>
-      <ContainerDiv ref={side} isMenuOpen={isMenuOpen}>
-        <div>
-          <MyInfoTextDiv>
-            Hello, Lenini <br /> Happy Unbirthday!
-          </MyInfoTextDiv>
-          <ProfileIcon />
-        </div>
-        {/* GO TO MY SESSION */}
-        <ContentDiv>
-          <GoToMySessionDiv>{/* 배경 이미지 넣어야 함 */}</GoToMySessionDiv>
-        </ContentDiv>
-        {/* 작성한 편지 */}
-        <ContentDiv>
-          <p>작성한 편지</p>
-          <div>
-            {/* 서버 작업 후 반복문으로, 리스트 정렬 기준 필요, DB에 편지 다 쓴 상태에 대한 필드 추가 필요 */}
-            <LetterContainerDiv background={'#FF6262'}>
-              <LetterDiv>
-                <LetterDoneIcon />
-                <LetterTextSpan>hihih monday</LetterTextSpan>
-              </LetterDiv>
-            </LetterContainerDiv>
-            <LetterContainerDiv background={'#FF9797'}>
-              <LetterDiv>
-                <LetterDoneIcon />
-                <LetterTextSpan>hihih dskljad</LetterTextSpan>
-              </LetterDiv>
-            </LetterContainerDiv>
-            <LetterContainerDiv background={'#FFC3C3'}>
-              <LetterDiv>
-                <LetterDoneIcon />
-                <LetterTextSpan>hihih kwkwho</LetterTextSpan>
-              </LetterDiv>
-            </LetterContainerDiv>
-            <LetterContainerDiv background={'#F1F1F1'}>
-              <LetterDiv>
-                <LetterIcon />
-                <LetterTextSpan>hihih kwkwho</LetterTextSpan>
-              </LetterDiv>
-            </LetterContainerDiv>
-            <LetterContainerDiv background={'#F1F1F1'}>
-              <LetterDiv>
-                <LetterIcon />
-                <LetterTextSpan>hihih kwkwho</LetterTextSpan>
-              </LetterDiv>
-            </LetterContainerDiv>
-            <LetterContainerDiv background={'#F1F1F1'}>
-              <LetterDiv>
-                <LetterIcon />
-                <LetterTextSpan>hihih kwkwho</LetterTextSpan>
-              </LetterDiv>
-            </LetterContainerDiv>
-          </div>
-        </ContentDiv>
-        {/* SUPPORT US */}
-        <ContentDiv>
-          <SupportUsContainerDiv>
-            <SupportIcon />
-          </SupportUsContainerDiv>
-        </ContentDiv>
-        {/* AD */}
-        <ContentDiv>
-          <AdDiv>AD</AdDiv>
-        </ContentDiv>
-      </ContainerDiv>
-    </div>
-  );
+type MenuProps = {
+  open: boolean;
 };
 
-const MenuButtonSpan = styled.span`
-  padding: 3%;
-  display: flex;
-  flex-direction: row-reverse;
-`;
+export const Menu: React.FC<MenuProps> = ({ open }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  useEffect(() => {
+    setIsMenuOpen(open);
+  });
 
-const ContainerDiv = styled.div<{ isMenuOpen: boolean }>`
+  console.log('@menu: isMenuOpen: ' + isMenuOpen);
+
+  return isMenuOpen ? <MenuContainer> </MenuContainer> : <div></div>;
+};
+
+const MenuContainer = styled.div`
+  color: white;
+  background: #334458;
   position: absolute;
-  width: 260px;
-  height: calc(100% - 40px);
-  top: 0;
-  right: -300px;
-  -webkit-transform: ${props => (props.isMenuOpen ? 'translateX(-300px)' : 'translateX(0)')};
-  transform: ${props => (props.isMenuOpen ? 'translateX(-300px)' : 'translateX(0)')};
-  -webkit-transition: 0.3s ease all;
-  transition: 0.3s ease all;
-  background: white;
-  z-index: 999;
-  padding: 20px;
-`;
-
-const MyInfoTextDiv = styled.div`
-  width: 70%;
-  height: 60px;
-  float: left;
-  padding-top: 10px;
-`;
-
-const ContentDiv = styled.div`
-  margin-top: 30px;
-`;
-
-const GoToMySessionDiv = styled.div`
-  border-radius: 20px;
-  background: #d9d9d9;
+  top: 0%;
+  display: flex;
+  z-index: 888;
   width: 100%;
-  height: 160px;
-`;
-
-const LetterContainerDiv = styled.div<{ background: string }>`
-  border-radius: 10px;
-  border: 2px solid white;
-  width: 95%;
-  height: 35px;
-  background: ${props => props.background};
-  padding: 5px;
-  margin-bottom: 5px;
-`;
-
-const LetterDiv = styled.div`
-  display: table;
-`;
-
-const LetterTextSpan = styled.span`
-  display: table-cell;
-  vertical-align: middle;
-`;
-
-const SupportUsContainerDiv = styled.div`
-  padding-left: 3%;
-`;
-
-const AdDiv = styled.div`
-  background: #d0d0d0;
-  width: 100%;
-  height: 60px;
-  text-align: center;
-  align-items: center;
+  height: 100%;
 `;
