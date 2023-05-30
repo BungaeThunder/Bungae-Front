e9db24597d75f3bb2bf84af07976736854d7b3b9
Hakjun Lee <haakjunlee@gmail.com>
2023-05-31 01:21:48 +0900

    wip: naverlogin

--- component/auth/LoginWithNaver.tsx
+++ component/auth/LoginWithNaver.tsx
@@ -1,37 +1,20 @@
-import Script from 'next/script';
 import LoginWithNaverIcon from 'public/images/auth/login_with_naver_account.svg';
-import { useRef } from 'react';
-import styled from 'styled-components';
 import StyledLoginButton from './StyledLoginButton';
 
 const LoginWithNaver = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const handleClick = () => (ref.current?.children[0] as HTMLElement).click();
-  const initLogin = () => {
-    const login = new window.naver.LoginWithNaverId({
-      clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
-      callbackUrl: `${process.env.NEXT_PUBLIC_SERVER_URL}/cake`,
-      callbackHandle: true,
-      isPopup: false,
-      loginButton: { color: 'white', type: 3, height: 50 },
-    });
-
-    login.init();
+  const callNaverLoginApi = async () => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/signUp/naver`);
+    } catch (error) {}
   };
 
   return (
     <>
-      <Script src="/sdk/naveridlogin_js_sdk_2.0.2.js" strategy="lazyOnload" onLoad={initLogin} />
-      <NaverIdLogin ref={ref} id="naverIdLogin" />
-      <StyledLoginButton onClick={handleClick}>
+      <StyledLoginButton onClick={callNaverLoginApi}>
         <LoginWithNaverIcon />
       </StyledLoginButton>
     </>
   );
 };
-
-const NaverIdLogin = styled.div`
-  display: none;
-`;
 
 export default LoginWithNaver;
