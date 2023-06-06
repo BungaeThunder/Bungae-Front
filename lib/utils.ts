2228d4100ec58143d1e7952e0bf84cd117ad84f1
jinwoo.baek <jinwoo.baek@linecorp.com>
2023-06-06 16:48:53 +0900

    feat: add axios util

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,15 +3,15 @@ import axios from 'axios';
 import PathEnum from 'paths';
 
 export const sleep = (ms: number) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 };
 
 const Api = axios.create({
-    baseURL: PathEnum.API_URL + '/',
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-    },
+  baseURL: PathEnum.API_URL + '/',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 });
 
 // TODO: 토큰 구현시 아래 코드 추가
