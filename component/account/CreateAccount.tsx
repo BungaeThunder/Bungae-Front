b70d36d27ab2059070515bb141223d2f803f5a78
gyurinida <gyurinida@naver.com>
2023-06-21 23:00:54 +0900

    #35 계정 생성하기 화면

--- component/account/CreateAccount.tsx
+++ component/account/CreateAccount.tsx
@@ -2,10 +2,13 @@ import * as React from 'react';
 import { Box, Stack, Input, FormControlLabel, Checkbox, Button } from '@mui/material';
 
 const ariaLabel = { 'aria-label': 'description' };
+const formBoxStyle = { padding: '5%' };
+const formControlLabelStyle = { mt: '3%' };
+const bosxStyle = { mx: 'auto', mt: '5%', width: 200 };
 
 export const CreateAccount = () => {
   return (
-    <Box component="form" sx={{ padding: '5%' }} autoComplete="off">
+    <Box component="form" sx={formBoxStyle} autoComplete="off">
       <h1>계정 생성하기</h1>
       <Stack spacing={2}>
         <Input placeholder="이름" inputProps={ariaLabel} />
@@ -13,11 +16,11 @@ export const CreateAccount = () => {
         <Input placeholder="Email" inputProps={ariaLabel} />
       </Stack>
       <FormControlLabel
-        sx={{ mt: '3%' }}
+        sx={formControlLabelStyle}
         control={<Checkbox />}
         label="생일 알림을 위한 이메일 수신에 동의합니다."
       />
-      <Box sx={{ mx: 'auto', mt: '5%', width: 200 }}>
+      <Box sx={bosxStyle}>
         <Button variant="contained" sx={{ width: 200 }}>
           나만의 케이크 만들기
         </Button>
